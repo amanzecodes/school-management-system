34049ab06cc3780024003f05ed73599071cb1af1
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-08-21 18:05:30 +0100

    removed some extra unneccesary code from the auth.route.ts

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -3,9 +3,9 @@ import { AuthController } from "../controllers/auth.controllers";
 
 const router = express.Router();
 
-router.post("/login", (req, res) => AuthController.Login(req, res));
-router.post("/adduser", (req, res) => AuthController.AddUser(req, res));
-router.post("/refresh", (req, res) => AuthController.RefreshToken(req, res));
-router.post("/logout", (req, res) => AuthController.Logout(req, res));
+router.post("/login", AuthController.Login);
+router.post("/adduser", AuthController.AddUser);
+router.post("/refresh", AuthController.RefreshToken);
+router.post("/logout", AuthController.Logout);
 
 export default router;
