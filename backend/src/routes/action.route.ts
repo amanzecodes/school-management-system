905d59309de9e4c7c802a5539c1113e283e50214
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-08-23 23:23:51 +0100

    added middleware for the single uplaod route in the action route

--- backend/src/routes/action.route.ts
+++ backend/src/routes/action.route.ts
@@ -1,8 +1,14 @@
-import express from "express"
+import express from "express";
 import { ActionController } from "../controllers/action.controllers";
+import { protectRoute, requireRole } from "../middleware/middleware";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post("single/:id", ActionController.SingleUpload)
+router.post(
+  "/single/:userId/:subjectId",
+  protectRoute,
+  requireRole("TEACHER"),
+  (req, res) => ActionController.SingleUpload(req as any, res as any)
+);
 
 export default router;
